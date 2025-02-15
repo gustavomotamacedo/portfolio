528db7dc9c379b0012b45d9b55dfa62536fc3f9d
gustavomotamacedo <mota.macedo05@gmail.com>
2025-02-15 13:49:36 -0300

    create footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { SkillsComponent } from './skills/skills.component';
 import { ExperienceComponent } from "./experience/experience.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, SkillsComponent, ExperienceComponent],
+  imports: [RouterOutlet, HomeComponent, SkillsComponent, ExperienceComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
